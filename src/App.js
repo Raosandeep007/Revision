610f6220dd986e9a16e80f05295304b9d02a3ac5
Raosandeep007 <yadavsandeep775@gmail.com>
2022-03-15 19:54:48 +0530

    4 files modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import { Counter } from './Counter/Counter';
 import { Todo } from './Todo/Todo';
+
 export default function App() {
   return (
     <div id="maindiv">
